a35f447a3d0c71a0e76617237990ad077e0ffca6
Galdea <gabriel.aldea@gmail.com>
2022-12-07 13:41:08 +0000

    final. me gustaria agregar regex a los inputs para el futuro

--- src/app.js
+++ src/app.js
@@ -18,42 +18,52 @@ const comment = document.getElementById("comment");
 
 form.addEventListener("submit", e => {
   let messages = [];
+
+  //ccn
   if (ccn.value === "" || ccn.value == null) {
     messages.push("Credit Card number required");
     ccn.classList.add("alert-danger");
   }
+  //cvc
   if (cvc.value === "" || cvc.value == null) {
     messages.push("CVC number required");
     cvc.classList.add("alert-danger");
   }
+  //money
   if (money.value === "" || money.value == null) {
     messages.push("Amount required");
     money.classList.add("alert-danger");
   }
+  //firstname
   if (firstname.value === "" || firstname.value == null) {
     messages.push("Name required");
     firstname.classList.add("alert-danger");
   }
+  //lastname
   if (lastname.value === "" || lastname.value == null) {
     messages.push("Last name required");
     lastname.classList.add("alert-danger");
   }
+  //city
   if (city.value === "" || city.value == null) {
-    messages.push("Last name required");
+    messages.push("City required");
     city.classList.add("alert-danger");
   }
-  // if (state.value === "" || state.value == null) {
-  //   messages.push("State required");
-  //   state.classList.add("alert-danger");
-  // }
-  if (zip.value === "" || zip.value == null) {
+  if (state.value == "..." || state.value == "null") {
     messages.push("State required");
-    zip.classList.add("alert-danger");
+    state.classList.add("alert-danger");
+  }
+
+  //zip
+  if (zip.value === "" || zip.value == null) {
+    messages.push("Zip required");
   }
 
   if (messages.length > 0) {
     e.preventDefault();
     errorElement.innerText = messages.join(", ");
     comment.classList.add("alert-danger");
+    zip.classList.add("alert-danger");
+    errorElement.style.display = "block";
   }
 });
