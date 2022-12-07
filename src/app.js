a0c210ed77ce064eb250f872386fd62a083352dc
Galdea <gabriel.aldea@gmail.com>
2022-12-07 02:21:52 +0000

    falta hide alert, numeros maximos en 4 inputs, stateinput, hacer que alerts salgan en input

--- src/app.js
+++ src/app.js
@@ -2,10 +2,58 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// create a couple of elements in an otherwise empty HTML page
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+const ccn = document.getElementById("ccn");
+const cvc = document.getElementById("cvc");
+const money = document.getElementById("money");
+const city = document.getElementById("city");
+const state = document.getElementById("inputState");
+const zip = document.getElementById("inputZip");
+const comment = document.getElementById("comment");
+
+form.addEventListener("submit", e => {
+  let messages = [];
+  if (ccn.value === "" || ccn.value == null) {
+    messages.push("Credit Card number required");
+    ccn.classList.add("alert-danger");
+  }
+  if (cvc.value === "" || cvc.value == null) {
+    messages.push("CVC number required");
+    cvc.classList.add("alert-danger");
+  }
+  if (money.value === "" || money.value == null) {
+    messages.push("Amount required");
+    money.classList.add("alert-danger");
+  }
+  if (firstname.value === "" || firstname.value == null) {
+    messages.push("Name required");
+    firstname.classList.add("alert-danger");
+  }
+  if (lastname.value === "" || lastname.value == null) {
+    messages.push("Last name required");
+    lastname.classList.add("alert-danger");
+  }
+  if (city.value === "" || city.value == null) {
+    messages.push("Last name required");
+    city.classList.add("alert-danger");
+  }
+  // if (state.value === "" || state.value == null) {
+  //   messages.push("State required");
+  //   state.classList.add("alert-danger");
+  // }
+  if (zip.value === "" || zip.value == null) {
+    messages.push("State required");
+    zip.classList.add("alert-danger");
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(", ");
+    comment.classList.add("alert-danger");
+  }
+});
